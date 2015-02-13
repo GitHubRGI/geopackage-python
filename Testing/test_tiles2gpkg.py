aff68d2c54075d9796941bb5c20885fa1530c40e
Steven D. Lander <steven.lander@rgi-corp.com>
2015-02-13 15:53:25 -0500

    Adding version hooks

--- Testing/test_tiles2gpkg.py
+++ Testing/test_tiles2gpkg.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python2.7
 """
-    Copyright (C) 2014 Reinventing Geospatial, Inc
+Copyright (C) 2014 Reinventing Geospatial, Inc.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>,
-    or write to the Free Software Foundation, Inc., 59 Temple Place -
-    Suite 330, Boston, MA 02111-1307, USA.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>,
+or write to the Free Software Foundation, Inc., 59 Temple Place -
+Suite 330, Boston, MA 02111-1307, USA.
 
-    Authors:
-        Steven D. Lander
-        Jason Hall
-        Jenifer Cochran
-    Date: 2014-08
-    Requires: Python Imaging Library (PIL or Pillow), Sqlite3
-    Description: Test cases for tiles2gpkg_parallel.py
+Authors:
+    Steven D. Lander
+    Jason Hall
+    Jenifer Cochran
+Date: 2014-08
+Requires: Python Imaging Library (PIL or Pillow), Sqlite3
+Description: Test cases for tiles2gpkg_parallel.py
+
+Version:
 """
 
 from math import pi
